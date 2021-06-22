8177c4a98576154597fcfedc1ae5485b24f83181
Subhrojoy Roy <subhrojoy_roy@yahoo.com>
2021-06-22 22:40:10 +0530

    Checking in the REST structure

--- src/main/java/com/metrorail/fare/ticketFare/entities/TigerCard.java
+++ src/main/java/com/metrorail/fare/ticketFare/entities/TigerCard.java
@@ -6,18 +6,21 @@ import java.util.List;
 
 public class TigerCard implements Card {
     private List<Journey> journeyList;
+    int cardId;
 
-    public TigerCard(List<Journey> journeyList) {
-        this.journeyList = journeyList;
+    public TigerCard(int cardId) {
+        this.cardId = cardId;
     }
 
     @Override
     public List<Journey> getTravelStatusOfToday() throws FareException {
+        // This weeks journey against this card will be fetched from DB
         return null;
     }
 
     @Override
     public List<Journey> getTravelStatusOfThisWeek() throws FareException {
+        // This weeks journey against this card will be fetched from DB
         return null;
     }
 }
--- src/main/java/com/metrorail/fare/ticketFare/services/impl/FareServiceImpl.java
+++ src/main/java/com/metrorail/fare/ticketFare/services/impl/FareServiceImpl.java
@@ -1,20 +1,24 @@
 package com.metrorail.fare.ticketFare.services.impl;
 
 import com.metrorail.fare.ticketFare.entities.Journey;
+import com.metrorail.fare.ticketFare.entities.TigerCard;
 import com.metrorail.fare.ticketFare.services.FareService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
 public class FareServiceImpl extends FareService {
+    @Autowired
+    TigerCard card;
+
     @Override
-    public int getFare(List<Journey> journeyList) {
+    public int getFare(int cardId, List<Journey> journeyList) {
+        card = new TigerCard(cardId);
         int fare = -1;
 
-        for(Journey journey : journeyList) {
-            fare += journey.getFromZone().getZoneId() - journey.getToZone().getZoneId();
-        }
+        // Logic of calculating fare
 
         return fare;
     }
